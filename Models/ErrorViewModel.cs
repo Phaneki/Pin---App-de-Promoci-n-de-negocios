7a57bc84dd8558f1831c13a1d39b7a7308482634
Phaneki <sjesuspintado@gmail.com>
2026-03-28 23:29:33 -0500

    Inicio

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Pin___App_de_Promoci_n_de_negocios.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
