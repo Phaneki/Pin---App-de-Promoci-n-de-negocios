18a79ef5618ad9e73eed6b6500834f822bf19763
Daliaga-c <diegoaliaga49@gmail.com>
2026-03-29 16:16:24 -0500

    Funcionalidad de la pagina explorar + filtro

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,6 +16,59 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Explorar()
+    {
+        var negocios = new List<Negocio>
+        {
+            new Negocio
+            {
+                Id = 1,
+                Nombre = "La Picantería del Sur",
+                Categoria = "Restaurantes",
+                Direccion = "Av. Larco 1230, Miraflores",
+                Calificacion = 4.8,
+                EstaAbierto = true,
+                Destacado = true,
+                ImagenUrl = "~/images/picanteria.jpg"
+            },
+            new Negocio
+            {
+                Id = 2,
+                Nombre = "Tienda Más Por Menos",
+                Categoria = "Tiendas",
+                Direccion = "Calle Las Ciencias 55, San Isidro",
+                Calificacion = 4.3,
+                EstaAbierto = false,
+                Destacado = false,
+                ImagenUrl = ""
+            },
+            new Negocio
+            {
+                Id = 3,
+                Nombre = "AutoServicio Rápido",
+                Categoria = "Supermercados",
+                Direccion = "Av. La Marina 980, Surco",
+                Calificacion = 4.5,
+                EstaAbierto = true,
+                Destacado = false,
+                ImagenUrl = null
+            },
+            new Negocio
+            {
+                Id = 4,
+                Nombre = "Café de la Esquina",
+                Categoria = "Cafés",
+                Direccion = "Calle 7 777, Miraflores",
+                Calificacion = 4.9,
+                EstaAbierto = true,
+                Destacado = true,
+                ImagenUrl = "~/images/cafe.jpg"
+            }
+        };
+
+        return View(negocios);
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
---
+++ Models/Negocio.cs
@@ -0,0 +1,13 @@
+namespace Pin___App_de_Promoci_n_de_negocios.Models;
+
+public class Negocio
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; } = string.Empty;
+    public string Categoria { get; set; } = string.Empty;
+    public string Direccion { get; set; } = string.Empty;
+    public double Calificacion { get; set; }
+    public bool EstaAbierto { get; set; }
+    public bool Destacado { get; set; }
+    public string ImagenUrl { get; set; } = "~/images/picanteria.jpg";
+}
