3c6c235792b3d1c87b261985e635aef5b7dbab39
Phaneki <sjesuspintado@gmail.com>
2026-04-18 17:17:24 -0500

    Incoporando DB en Explorar

--- Models/Negocio.cs
+++ Models/Negocio.cs
@@ -3,6 +3,7 @@ public class Negocio
 {
     public int Id { get; set; }
     public string Nombre { get; set; } = string.Empty;
+
     public string Categoria { get; set; } = string.Empty;
     public string Direccion { get; set; } = string.Empty;
     public double Calificacion { get; set; }
